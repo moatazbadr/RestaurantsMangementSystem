97dfd3ff6499b25eab7b9d69b71dd594dafd9353
moatazbadr <zezobadr988@gmail.com>
2025-10-02 10:31:10 +0300

    first Upload

---
+++ Restaurants.API/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+if (app.Environment.IsDevelopment())
+{
+//    app.useSwagger();
+//    app.useSwaggerUI();
+
+}
+
+app.Run();
