190916355edb4adfc882b4f83d370a930519615f
moatazbadr <zezobadr988@gmail.com>
2025-10-05 12:00:17 +0300

    Adding DbContext class

---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+public class RestaurantsDbContext : DbContext
+{
+    //انا غبي بس عديها معلش
+    internal DbSet<Restaurant.Domain.Entities.Restaurant> restaurants { get; set; }
+
+}
