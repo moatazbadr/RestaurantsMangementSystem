2f062b75b61d5871fa4a952c386f5f1b32a0046e
moatazbadr <zezobadr988@gmail.com>
2025-10-17 10:17:00 +0300

    Adding FluentValidation

--- Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
+++ Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,21 @@ namespace Restaurants.Application.Restaurants.Dtos;
 
 public class CreateRestaurantDto
 {
+    [StringLength(100,MinimumLength =3)]
     public string Name { get; set; } = default!;
     public string Description { get; set; } = default!;
+    [Required(ErrorMessage ="Enter a valid category")]
     public string Category { get; set; } = default!;
     public bool HasDelivery { get; set; }
 
+    [EmailAddress(ErrorMessage ="Please enter a valid email address")]
     public string? ContactEmail { get; set; }
+    [Phone(ErrorMessage ="Please enter a valid contact number")]
     public string? ContactNumber { get; set; }
 
     public string? City { get; set; }
     public string? Street { get; set; }
+    [RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage = "Postal code must be in the format XX-XXX")]
     public string? PostalCode { get; set; }
 
 }
