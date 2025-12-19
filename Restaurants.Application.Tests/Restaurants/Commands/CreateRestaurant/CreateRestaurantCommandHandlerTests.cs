b70add904970e8c2bbb95dd88401d17ebfd40fcb
moatazbadr <zezobadr988@gmail.com>
2025-12-19 16:41:10 +0200

    after pipeline

--- Restaurants.Application.Tests/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandlerTests.cs
+++ Restaurants.Application.Tests/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandlerTests.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
+using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Restaurant.Domain.Entities;
 using Restaurant.Domain.Repositories;
 using Restaurants.Application.User;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Restaurants.Application.Restaurants.Commands.RestaurantCommand.Tests;
@@ -11,15 +13,21 @@ namespace Restaurants.Application.Restaurants.Commands.RestaurantCommand.Tests;
 public class CreateRestaurantCommandHandlerTests
 {
     [Fact()]
-    public void Handle_ForValidCommands_returnsRestaurantDto()
-    {
+    public async Task Handle_ForValidCommands_returnsRestaurantDto()
+     {
         //arrange 
         //mocks
         var IloggerMock = new Mock<ILogger<CreateRestaurantCommandHandler>>();
         var IrestaurantRepositoryMock = new Mock<IRestaurantRepository>();
         IrestaurantRepositoryMock.Setup(repo=> repo.CreateRestaurantAsync(It.IsAny<RestaurantsEntity>()))
             .ReturnsAsync(new RestaurantsEntity { Id = 1, Name = "Test Restaurant" });
+
+
         var ImapperMock = new Mock<IMapper>();
+        var command = new CreateRestaurantCommand();
+        var restaurant = new RestaurantsEntity { Id = 1, Name = "Test Restaurant" };
+        ImapperMock.Setup(m => m.Map<RestaurantsEntity>(command)).Returns(restaurant);
+        
         var IuserContextMock = new Mock<IUserContext>();
 
         var user = new CurrentUser("userId-123", "Example@test.com", [], null, null);
@@ -30,11 +38,12 @@ public class CreateRestaurantCommandHandlerTests
 
 
         //act
-
+        var result = await commandHandler.Handle(command, CancellationToken.None);
 
 
 
         //assert
-
+        result.Should().NotBeNull();
+        restaurant.ownerId.Should().Be(user.UserId);
     }
 }
